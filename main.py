b22536f9ffbddb094f70585ecd91de9c6e52b53c
jpschuh19 <83608880+jpschuh19@users.noreply.github.com>
2021-10-20 22:45:56 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+for line in sys.stdin:
+    if 'q' == line.rstrip():
+        break
+    with open(line) as f:
+        names = f.readlines()
+
+names.sort()
+names.sort(key=len, reverse=True)
+
+def print_to_stdout(a):
+    print(a, file=sys.stdout)
+
+def print_forward():
+    for i in names:
+        print_to_stdout(names[i])
+
+def print_reverse():
+    i=len(names)-1
+    while(i>=0):
+        print_to_stdout(names[i])
+        i+=1
+
+
